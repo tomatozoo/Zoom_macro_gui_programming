0f260b21b664912c6b5246113ac4378dcbc5299c
Young Zoo Park <enkeejuniour@naver.com>
2022-01-25 13:13:07 +0900

    GUI 프로그래밍 : with initial UI

---
+++ GUI.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('ZOOM 알림 프로그램')
+        
+        btn = QPushButton('Quit', self)
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)  
+        
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
