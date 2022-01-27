3ddeb3b150522228d56449d93e21630e3277a039
Young Zoo Park <enkeejuniour@naver.com>
2022-01-27 20:28:23 +0900

    학교 작업 마지막 (time 반영하기)

--- GUI.py
+++ GUI.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import QCoreApplication, QDate, Qt
 from PyQt5 import QtSql
+import time
 
 class DB():
     def __init__(self):
